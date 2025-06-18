083e6a4a2672aac5261eea45a35b4841ebf7b593
rizalsuryana <rizal.suryn@gmail.com>
2025-06-19 06:20:53 +0700

    add edit, delete product

---
+++ app/products/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+import ProductForm from '@/components/ProductForm';
+import { getProduct, updateProduct } from '@/lib/api/products';
+import { redirect } from 'next/navigation';
+
+export default async function EditProductPage({params} : {params: {id: string}}) {
+    const product = await getProduct(params.id);
+
+    const handleUpdate = async (data: any) => {
+        'use server';
+        await updateProduct(params.id, data);
+        redirect('/products');
+    }
+  return (
+    <div>
+        <ProductForm initial={product} onSubmit={handleUpdate}/>
+    </div>
+  );
+}
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,23 @@
-type ProductPageProps = {
-    params: Promise<{id: string}>
-}
+import { getProduct, deleteProduct } from "@/lib/api/products";
+import { redirect } from "next/navigation";
 
-export default async function ProductPage({ params }: ProductPageProps){
-    const {id} = await params;
-    console.log(id);
-    return(
-        <div>
-            Product Detail Page
-        </div>
-    )
+export default async function ProductDetail({params}: { params: {id: string}}) {
+    const product = await getProduct(params.id);
+    async function handleDelete() {
+        'use server';
+        await deleteProduct(params.id);
+        redirect('/products');
+        
+    }
+  return (
+    <div>
+        <h1>
+            {product.name}
+            <p>${product.price}</p>
+            <form action={handleDelete}>
+                <button type="submit">Delete</button>
+            </form>
+        </h1>
+    </div>
+  );
 }
